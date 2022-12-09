745aa7a14245132d5137d53da581db16853fdc73
pongpob <khlangs456@gmail.com>
2022-12-09 16:47:02 +0700

    FinalQ1

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Program
+{
+    
+    
+    static void Main()
+    {
+        
+        
+        Stack<string> stack = new Stack<string>(); 
+        string input;
+        int inputint;
+        
+        input = Console.ReadLine();
+        
+        
+        
+        
+        stack.Push(input);
+
+        
+        if(input == ")")
+
+        {
+            
+            
+            Main();
+
+            
+
+            
+            
+            
+        }
+
+        else if(input == "(")
+        {
+            
+            Main();
+
+           
+
+            
+
+
+            
+            
+
+        }
+        else
+        {
+            
+        }
+        
+
+        
+    
+    }
+    
+       
+}
